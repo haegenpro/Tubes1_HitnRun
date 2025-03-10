ace7de212e93a09dc1671c30e53c093978af12b4
Haegen <19623009@std.stei.itb.ac.id>
2025-03-10 13:33:38 +0700

    Update WildBot binaries and assembly information

--- src/alternative_bots/WildBot/obj/Debug/net6.0/WildBot.AssemblyInfo.cs
+++ src/alternative_bots/WildBot/obj/Debug/net6.0/WildBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WildBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8fd15c378faa60ea576b6981fc10bd0e15694e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6b8a20cc366a8ab1e010590c4b32380715c544")]
 [assembly: System.Reflection.AssemblyProductAttribute("WildBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WildBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
